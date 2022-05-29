74e4c9c0b2abc0b64e740e43603f5e448f865af8
Maor <maor@magori.online>
2022-05-29 22:54:26 +0300

    fix: 🔧 Change app's config to populate with config.py
    Fix the uses of config in opengate and config now loads from .env file

--- OpenSesame/config.py
+++ OpenSesame/config.py
@@ -1,7 +1,11 @@
 from os import environ as env
+from dotenv import load_dotenv
+
 
 # Statement for enabling the development environment
 DEBUG = True if env.get('ENV', 'DEVELOPMENT') == 'DEVELOPMENT' else False
 
 # The Arduino path on my machine.
 ARDUINO_PATH = env.get('ARDUINO_PATH', '/dev/ttyACM0')
+
+FIREBASE_PROJECT_ID = env.get('FIREBASE_PROJECT_ID')
