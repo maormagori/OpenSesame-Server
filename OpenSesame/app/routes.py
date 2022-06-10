1103fcc87e8bd87d8d78945d09a1f39b3687560c
Maor <maor@magori.online>
2022-06-10 13:19:09 +0300

    feat: ✨ Add the route to update user roles

--- OpenSesame/app/routes.py
+++ OpenSesame/app/routes.py
@@ -20,3 +20,17 @@ def close_gate(claims, *args, **kwargs):
         return "Closing Gate!"
     else:
         flask.abort(403)
+
+
+@app.route("roles/add/<UUID:user_id>")
+def add_role(user_id):
+    ali_baba_id, roles = flask.request.args.get(
+        "uid", ""), flask.request.args.get("roles")
+
+    if not auth.is_ali_baba(ali_baba_id):
+        flask.abort(403)
+
+    if not auth.get_user_roles(user_id):
+        flask.abort(flask.Response("UID missing from database", 404))
+
+    auth.set_user_roles(user_id, roles, ali_baba_id)
