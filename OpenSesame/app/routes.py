45930f4cbe2c1cd8acb3bd7701dacb4ff535d5e5
Maor <maor@magori.online>
2022-06-10 14:01:18 +0300

    fix: 🐛 Added responses to add_role and fixed typo

--- OpenSesame/app/routes.py
+++ OpenSesame/app/routes.py
@@ -1,5 +1,6 @@
 import flask
 from app import app, opengate, auth
+import json
 
 
 @app.route("/open")
@@ -22,15 +23,22 @@ def close_gate(claims, *args, **kwargs):
         flask.abort(403)
 
 
-@app.route("roles/add/<UUID:user_id>")
+@app.route("/roles/add/<user_id>")
 def add_role(user_id):
-    ali_baba_id, roles = flask.request.args.get(
-        "uid", ""), flask.request.args.get("roles")
+    try:
+        ali_baba_id, roles = flask.request.args.get(
+            "uid", " "), flask.request.args.getlist("roles")
 
-    if not auth.is_ali_baba(ali_baba_id):
-        flask.abort(403)
+        if not auth.is_ali_baba(ali_baba_id):
+            flask.abort(403)
 
-    if not auth.get_user_roles(user_id):
-        flask.abort(flask.Response("UID missing from database", 404))
+        if not auth.get_user_roles(user_id):
+            flask.abort(flask.Response("UID missing from database", 404))
 
-    auth.set_user_roles(user_id, roles, ali_baba_id)
+        auth.set_user_roles(user_id, roles, ali_baba_id)
+
+        return json.dumps({'success': True, 'rolesAdded': roles}), 200, {'ContentType': 'application/json'}
+    except Exception as exception:
+        app.logger.error(
+            "An exception occured while trying to update user roles: ", exception)
+        return json.dumps({'success': False}), 500, {'ContentType': 'application/json'}
